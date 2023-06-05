d00f6be15a5355b4c0cfd6014f5f917b6e79248f
pieceowater <yury.lvn@mail.ru>
2023-06-05 23:56:22 +0600

    first commit

---
+++ MoneyGuard/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MoneyGuard
+//
+//  Created by yury mid on 05.06.2023.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("MoneyGuard")
+    }
+
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoneyGuard/MoneyGuardApp.swift
@@ -0,0 +1,18 @@
+//
+//  MoneyGuardApp.swift
+//  MoneyGuard
+//
+//  Created by yury mid on 05.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoneyGuardApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
